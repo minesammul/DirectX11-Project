52d79726a49702b33bad8080192fa9829aad00d5
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-07-18 21:34:51 +0900

    상수 버퍼의 생성과 GPU의 Register에 전달하여 셰이더에 사용하는 법

--- DirectX11/Engine/Core.cpp
+++ DirectX11/Engine/Core.cpp
@@ -11,6 +11,7 @@ TVertex g_arrVtx[4] = {};
 
 ID3D11Buffer * g_pVB = nullptr;
 ID3D11Buffer * g_pIB = nullptr;
+ID3D11Buffer* g_pCB = nullptr;
 
 ID3DBlob*	   g_pVtxBlob = nullptr;
 ID3DBlob*	   g_pPixelBlob = nullptr;
@@ -20,6 +21,11 @@ ID3D11VertexShader * g_pVtxShader = nullptr;
 ID3D11PixelShader * g_pPixelShader = nullptr;
 
 ID3D11InputLayout* g_pLayout = nullptr;
+
+DirectX::XMFLOAT3 g_vWorldPos = DirectX::XMFLOAT3(10.f, 0.f, 10.f);
+DirectX::XMFLOAT3 g_vWorldScale = DirectX::XMFLOAT3(1.f, 1.f, 1.f);
+DirectX::XMFLOAT3 g_vWorldRotation = DirectX::XMFLOAT3(0.f, 0.f, 0.f);
+
 
 Core::Core()
 {
@@ -41,6 +47,7 @@ Core::~Core()
 	g_pLayout->Release();
 
 	g_pVB->Release();
+	g_pCB->Release();
 }
 
 int Core::Init(HWND hWnd, bool bWindowed)
@@ -99,6 +106,14 @@ int Core::Init(HWND hWnd, bool bWindowed)
 	tSub.pSysMem = arrIdx; // 전역변수에 저장해둔 정점 정보를 GPU 메모리 초기값으로 전달
 
 	Device::GetInstance()->GetDevice()->CreateBuffer(&tBufferDesc, &tSub, &g_pIB);
+
+	//Create Constant Buffer
+	tBufferDesc.ByteWidth = sizeof(DirectX::XMMATRIX) * 3;
+	tBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	tBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	tBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+	Device::GetInstance()->GetDevice()->CreateBuffer(&tBufferDesc, nullptr, &g_pCB);
 
 
 	// Shader 만들기
@@ -216,41 +231,40 @@ void Core::Update()
 
 	if (KEYHOLD(KEY_TYPE::KEY_LEFT))
 	{
-		g_arrVtx[0].vPos.x -= 0.5f * DT;
-		g_arrVtx[1].vPos.x -= 0.5f * DT;
-		g_arrVtx[2].vPos.x -= 0.5f * DT;
-		g_arrVtx[3].vPos.x -= 0.5f * DT;
+		g_vWorldPos.x -= 100.f * DT;
 	}
 
 	if (KEYHOLD(KEY_TYPE::KEY_RIGHT))
 	{
-		g_arrVtx[0].vPos.x += 0.5f * DT;
-		g_arrVtx[1].vPos.x += 0.5f * DT;
-		g_arrVtx[2].vPos.x += 0.5f * DT;
-		g_arrVtx[3].vPos.x += 0.5f * DT;
+		g_vWorldPos.x += 100.f * DT;
 	}
 
 	if (KEYHOLD(KEY_TYPE::KEY_UP))
 	{
-		g_arrVtx[0].vPos.y += 0.5f * DT;
-		g_arrVtx[1].vPos.y += 0.5f * DT;
-		g_arrVtx[2].vPos.y += 0.5f * DT;
-		g_arrVtx[3].vPos.y += 0.5f * DT;
+		g_vWorldPos.y += 100.f * DT;
 	}
 
 	if (KEYHOLD(KEY_TYPE::KEY_DOWN))
 	{
-		g_arrVtx[0].vPos.y -= 0.5f * DT;
-		g_arrVtx[1].vPos.y -= 0.5f * DT;
-		g_arrVtx[2].vPos.y -= 0.5f * DT;
-		g_arrVtx[3].vPos.y -= 0.5f * DT;
+		g_vWorldPos.y -= 100.f * DT;
 	}
+	DirectX::XMMATRIX matScale = DirectX::XMMatrixScaling(g_vWorldScale.x, g_vWorldScale.y, g_vWorldScale.z);
+	DirectX::XMMATRIX matRotationX = DirectX::XMMatrixRotationX(g_vWorldRotation.x);
+	DirectX::XMMATRIX matRotationY = DirectX::XMMatrixRotationY(g_vWorldRotation.y);
+	DirectX::XMMATRIX matRotationZ = DirectX::XMMatrixRotationZ(g_vWorldRotation.z);
+	DirectX::XMMATRIX matTrans = DirectX::XMMatrixTranslation(g_vWorldPos.x, g_vWorldPos.y, g_vWorldPos.z);
+
+	DirectX::XMMATRIX matWorld = matScale * matRotationX * matRotationY * matRotationZ * matTrans;
+
 
 	D3D11_MAPPED_SUBRESOURCE tSub = {};
 
-	Device::GetInstance()->GetContext()->Map(g_pVB, 0, D3D11_MAP_WRITE_DISCARD, 0, &tSub);
-	memcpy(tSub.pData, g_arrVtx, sizeof(TVertex) * 4);
-	Device::GetInstance()->GetContext()->Unmap(g_pVB, 0);
+	Device::GetInstance()->GetContext()->Map(g_pCB, 0, D3D11_MAP_WRITE_DISCARD, 0, &tSub);
+	memcpy(tSub.pData, &matWorld, sizeof(DirectX::XMMATRIX));
+	Device::GetInstance()->GetContext()->Unmap(g_pCB, 0);
+
+	// 상수 0번 레지스터 메모리에 Vertex Shader 시점에 해당 버퍼를 전달
+	Device::GetInstance()->GetContext()->VSSetConstantBuffers(0, 1, &g_pCB);
 }
 
 void Core::Render()
