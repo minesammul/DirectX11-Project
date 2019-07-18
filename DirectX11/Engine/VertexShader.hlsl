a8957cd39e26519a47c85f0dc7d03f6bb6833e26
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-07-18 21:35:16 +0900

    Revert "상수 버퍼의 생성과 GPU의 Register에 전달하여 셰이더에 사용하는 법"
    This reverts commit 52d79726a49702b33bad8080192fa9829aad00d5.

--- DirectX11/Engine/VertexShader.hlsl
+++ DirectX11/Engine/VertexShader.hlsl
@@ -1,8 +1,3 @@
-cbuffer MATRIX : register(b0)
-{
-    matrix g_mWorld;
-}
-
 struct VERTEX_INPUT
 {
     float3 vPos : POSITION;
@@ -11,7 +6,7 @@ struct VERTEX_INPUT
 
 struct VERTEX_OUTPUT
 {
-    float4 vPos : SV_POSITION;
+	float4 vPos : SV_Position;
 	float4 vColor : COLOR;
 };
 
@@ -19,9 +14,7 @@ struct VERTEX_OUTPUT
 VERTEX_OUTPUT main( VERTEX_INPUT input )
 {
 	VERTEX_OUTPUT output = (VERTEX_OUTPUT) 0.f;
-    
-    float4 vWorldPos = mul(float4(input.vPos, 1.f), g_mWorld);
-   
+
 	output.vPos = float4(input.vPos, 1.f);
 	output.vColor = input.vColor;
 
