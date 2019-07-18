2550a3392d074ad476abc64a870c0905b7703962
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-07-18 21:40:11 +0900

    Pixel Shader에서 Vertex Shader의 Semantic을 받지 못했던 에러 수정

--- DirectX11/Engine/PixelShader.hlsl
+++ DirectX11/Engine/PixelShader.hlsl
@@ -1,10 +1,10 @@
 struct PIXEL_INPUT
 {
-	float3 vPos : POSITION;
-	float4 vColor : COLOR;
+    float4 vPos : SV_Position;
+    float4 vColor : COLOR;
 };
 
-float4 main(PIXEL_INPUT input) : SV_TARGET
+float4 main(PIXEL_INPUT input) : SV_Target
 {
     input.vColor = float4(1.0f, 1.0f, 0.0f, 1.0f);
 	return input.vColor;
