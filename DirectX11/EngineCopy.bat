a0a4f06102be1689a54a9621bf99d86d127f16da
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-10-02 23:05:32 +0900

    헤더파일에 대한 변경점은 찾지 못해서 빌드마다 코드 복사되도록 bat파일 수정

--- DirectX11/EngineCopy.bat
+++ DirectX11/EngineCopy.bat
@@ -1,2 +1,2 @@
-xcopy /s /y /d /exclude:exclude_list.txt ".\02. Engine\*.h" ".\Project\Include\Engine"
-xcopy /s /y /d ".\02. Engine\*.fx" ".\bin\content\Shader"
+xcopy /s /y /exclude:exclude_list.txt ".\02. Engine\*.h" ".\Project\Include\Engine"
+xcopy /s /y ".\02. Engine\*.fx" ".\bin\content\Shader"
