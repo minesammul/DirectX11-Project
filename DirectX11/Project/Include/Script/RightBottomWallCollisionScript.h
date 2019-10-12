50b913bbb8b8044d6127f062b542e4e45f1dea89
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-10-13 00:54:26 +0900

    기타 리소스 업데이트

--- DirectX11/Project/Include/Script/RightBottomWallCollisionScript.h
+++ DirectX11/Project/Include/Script/RightBottomWallCollisionScript.h
@@ -3,8 +3,15 @@
 
 class CRightBottomWallCollisionScript : public CScript
 {
+private:
+	Vec3 collisionPosition;
+
 public:
 	CRightBottomWallCollisionScript();
 	~CRightBottomWallCollisionScript();
+
+public:
+	virtual void OnCollisionEnter(CCollider2D* _pOther);
+	virtual void OnCollision(CCollider2D* _pOther);
 };
 
