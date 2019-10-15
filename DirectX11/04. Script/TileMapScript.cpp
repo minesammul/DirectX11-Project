8750e045a4ed40563f1a03e331ceab77fddf261f
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-10-15 18:36:29 +0900

    TileMap 확대, 축소, 저장, 불러오기 기능

--- DirectX11/04. Script/TileMapScript.cpp
+++ DirectX11/04. Script/TileMapScript.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "TileMapScript.h"
 #include "TileScript.h"
-#include <iomanip>
+#include <Camera.h>
 
 
 CTileMapScript::CTileMapScript() : 
@@ -47,26 +47,58 @@ void CTileMapScript::awake()
 		}
 	}
 
+	//
 	CResPtr<CTexture> tileTexture = CResMgr::GetInst()->FindRes<CTexture>(L"Texture\\Tile\\No Animation Tile\\16x16\\16x16Tileset.png");
 	Object()->MeshRender()->GetSharedMaterial()->SetData(SHADER_PARAM::TEX_1, &tileTexture);
 
-	int inputGridCount = gridCount;
+	int inputGridCount = GRID_COUNT;
 	Object()->MeshRender()->GetSharedMaterial()->SetData(SHADER_PARAM::INT_0, &inputGridCount);
 
 	int textureTileCount = 487;
 	Object()->MeshRender()->GetSharedMaterial()->SetData(SHADER_PARAM::INT_1, &textureTileCount);
 
+	Object()->MeshRender()->GetSharedMaterial()->SetTileData(referecneTileIndexData);
+
+	//
+	vector<CCamera*> cameras = CSceneMgr::GetInst()->GetCurScene()->GetCamera();
+	for (int cameraIndex = 0; cameraIndex < cameras.size(); cameraIndex++)
+	{
+		if (cameras[cameraIndex]->IsValiedLayer(Object()->GetLayerIdx()))
+		{
+			tileMapCamera = cameras[cameraIndex];
+			break;
+		}
+	}
 }
 
 
 void CTileMapScript::update()
 {
-	/*tTileMap tileData = Object()->MeshRender()->GetSharedMaterial()->GetTileData();
-	for (int index = 0; index < 1024; index++)
-	{
-		referecneTileIndexData[index] = tileData.referenceTileIndex[index];
-	}*/
+	EditTileMap();
+	EditMoveTileMap();
+}
 
+void CTileMapScript::SaveToScene(FILE * _pFile)
+{
+	for (int index = 0; index < 1024; ++index)
+	{
+		int tileIndex = referecneTileIndexData[index];
+		fwrite(&tileIndex, sizeof(int), 1, _pFile);
+	}
+}
+
+void CTileMapScript::LoadFromScene(FILE * _pFile)
+{
+	for (int index = 0; index < 1024; ++index)
+	{
+		int tileIndex = -1;
+		fread(&tileIndex, sizeof(int), 1, _pFile);
+		referecneTileIndexData[index] = tileIndex;
+	}
+}
+
+void CTileMapScript::EditTileMap()
+{
 	CGameObject* curObject = Object();
 	if (tilesetScript != nullptr)
 	{
@@ -89,6 +121,9 @@ void CTileMapScript::update()
 					nowMousePosition.x = startMousePosition.x - CKeyMgr::GetInst()->GetMousePos().x;
 					nowMousePosition.y = startMousePosition.y - CKeyMgr::GetInst()->GetMousePos().y;
 
+					nowMousePosition.x *= tileMapCamera->GetScale();
+					nowMousePosition.y *= tileMapCamera->GetScale();
+
 					mousePickingPosition.vector4_f32[0] -= nowMousePosition.x;
 					mousePickingPosition.vector4_f32[1] += nowMousePosition.y;
 
@@ -98,8 +133,8 @@ void CTileMapScript::update()
 					gridX -= objectPosition.x;
 					gridY += objectPosition.y;
 
-					float gridSizeX = objectScale.x / gridCount;
-					float gridSizeY = objectScale.y / gridCount;
+					float gridSizeX = objectScale.x / GRID_COUNT;
+					float gridSizeY = objectScale.y / GRID_COUNT;
 
 					int selectGridColumn = gridX / gridSizeX;
 					int selectGridRow = gridY / gridSizeY;
@@ -108,18 +143,59 @@ void CTileMapScript::update()
 
 					int selectedTilesetIndex = curTileScript->GetSelectTileIndex();
 
-					int selectedGridIndex = selectGridColumn + selectGridRow * gridCount;
+					int selectedGridIndex = selectGridColumn + selectGridRow * GRID_COUNT;
 
 					referecneTileIndexData[selectedGridIndex] = selectedTilesetIndex;
 
-
-					//Object()->MeshRender()->GetSharedMaterial()->SetData(SHADER_PARAM::INT_2, &selectTileIndex);
 					Object()->MeshRender()->GetSharedMaterial()->SetTileData(referecneTileIndexData);
 				}
 			}
 		}
 
 	}
+}
 
+void CTileMapScript::EditMoveTileMap()
+{
+	if (tilesetScript != nullptr)
+	{
+		if (CKeyMgr::GetInst()->GetKeyState(KEY_TYPE::KEY_A) == KEY_STATE::STATE_HOLD)
+		{
+			Vec3 ObjectPosition = Object()->Transform()->GetLocalPos();
+			ObjectPosition.x -= TILE_MOVE_SPEED * DT;
+			Object()->Transform()->SetLocalPos(ObjectPosition);
+		}
+		else if (CKeyMgr::GetInst()->GetKeyState(KEY_TYPE::KEY_D) == KEY_STATE::STATE_HOLD)
+		{
+			Vec3 ObjectPosition = Object()->Transform()->GetLocalPos();
+			ObjectPosition.x += TILE_MOVE_SPEED * DT;
+			Object()->Transform()->SetLocalPos(ObjectPosition);
+		}
 
+		if (CKeyMgr::GetInst()->GetKeyState(KEY_TYPE::KEY_W) == KEY_STATE::STATE_HOLD)
+		{
+			Vec3 ObjectPosition = Object()->Transform()->GetLocalPos();
+			ObjectPosition.y += TILE_MOVE_SPEED * DT;
+			Object()->Transform()->SetLocalPos(ObjectPosition);
+		}
+		else if (CKeyMgr::GetInst()->GetKeyState(KEY_TYPE::KEY_S) == KEY_STATE::STATE_HOLD)
+		{
+			Vec3 ObjectPosition = Object()->Transform()->GetLocalPos();
+			ObjectPosition.y -= TILE_MOVE_SPEED * DT;
+			Object()->Transform()->SetLocalPos(ObjectPosition);
+		}
+
+		if (CKeyMgr::GetInst()->GetKeyState(KEY_TYPE::KEY_Q) == KEY_STATE::STATE_TAB)
+		{
+			float nowCameraScale = tileMapCamera->GetScale();
+			nowCameraScale *= TILE_SCALE_MULTI_VALUE;
+			tileMapCamera->SetScale(nowCameraScale);
+		}
+		else if (CKeyMgr::GetInst()->GetKeyState(KEY_TYPE::KEY_E) == KEY_STATE::STATE_TAB)
+		{
+			float nowCameraScale = tileMapCamera->GetScale();
+			nowCameraScale /= TILE_SCALE_MULTI_VALUE;
+			tileMapCamera->SetScale(nowCameraScale);
+		}
+	}
 }
--- DirectX11/04. Script/TileMapScript.h
+++ DirectX11/04. Script/TileMapScript.h
@@ -1,13 +1,19 @@
 #pragma once
 #include <Script.h>
+
+class CCamera;
 class CTileMapScript : public CScript
 {
 private:
 	int referecneTileIndexData[1024];
 
-	const int gridCount = 32;
+	const int GRID_COUNT = 32;
 	CScript* tilesetScript;
 	POINT startMousePosition;
+	const float TILE_MOVE_SPEED = 200.f;
+	const float TILE_SCALE_MULTI_VALUE = 1.2f;
+	CCamera* tileMapCamera;
+
 public:
 	CTileMapScript();
 	~CTileMapScript();
@@ -15,5 +21,10 @@ public:
 public:
 	virtual void awake();
 	virtual void update();
+	virtual void SaveToScene(FILE* _pFile);
+	virtual void LoadFromScene(FILE* _pFile);
+
+	void EditTileMap();
+	void EditMoveTileMap();
 };
 
