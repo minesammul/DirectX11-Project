54edeaec7ceb8f2f8d86055443a027a287b35ce2
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-10-23 01:56:42 +0900

    왼쪽벽과 밑의 벽이 동시에 충돌중일때 발생하는 버그 수정

--- DirectX11/04. Script/LeftWallCollisionScript.cpp
+++ DirectX11/04. Script/LeftWallCollisionScript.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "LeftWallCollisionScript.h"
+#include "BottomWallCollisionScript.h"
 
 
 CLeftWallCollisionScript::CLeftWallCollisionScript():
@@ -15,17 +16,36 @@ CLeftWallCollisionScript::~CLeftWallCollisionScript()
 void CLeftWallCollisionScript::OnCollisionEnter(CCollider2D * _pOther)
 {
 	collisionOn = true;
-
-	collisionPosition = Object()->GetParent()->Transform()->GetLocalPos();
 }
 
 void CLeftWallCollisionScript::OnCollision(CCollider2D * _pOther)
 {
-	//Vec3 characterPosition = Object()->GetParent()->Transform()->GetLocalPos();
-	//characterPosition.x = collisionPosition.x;
-	//Object()->GetParent()->Transform()->SetLocalPos(characterPosition);
+	bool isBottomWallCollision = false;
+	Vec3 bottomWallCollisionPosition = Vec3(0.f, 0.f, 0.f);
+	vector<CGameObject*> brotherObject = Object()->GetParent()->GetChild();
+	for (int brotherIndex = 0; brotherIndex < brotherObject.size(); brotherIndex++)
+	{
+		vector<CScript*> brotherScripts;
+		brotherScripts = brotherObject[brotherIndex]->GetScripts();
+
+		for (int scriptIndex = 0; scriptIndex < brotherScripts.size(); scriptIndex++)
+		{
+			if (brotherScripts[scriptIndex]->GetScriptType() == (UINT)SCRIPT_TYPE::BOTTOMWALLCOLLISIONSCRIPT)
+			{
+				CBottomWallCollisionScript* bottomWallCollisionScript = dynamic_cast<CBottomWallCollisionScript*>(brotherScripts[scriptIndex]);
+				isBottomWallCollision = bottomWallCollisionScript->GetCollision();
+				bottomWallCollisionPosition = bottomWallCollisionScript->GetCollisionPosition();
+			}
+		}
+	}
+
 
 	Vec3 characterPosition = Object()->GetParent()->Collider2D()->GetFinalPositon();
+	if (isBottomWallCollision == true)
+	{
+		characterPosition.y = bottomWallCollisionPosition.y;
+	}
+
 	Vec3 characterScale = Object()->GetParent()->Collider2D()->GetFinalScale();
 	characterPosition.x = _pOther->Object()->Transform()->GetLocalPos().x + (_pOther->Object()->Transform()->GetLocalScale().x / 2) + (characterScale.x / 2);
 	Object()->GetParent()->Transform()->SetLocalPos(characterPosition);
