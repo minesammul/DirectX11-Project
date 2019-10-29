a3a596f098a4ab5296b24cfc70f70ae401ef0ba7
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-10-29 23:59:07 +0900

    총알에 대한 EventQueue 연동.
    총알에 맞는 캐릭터들에 대해 통신하기 위한 것이다.

--- DirectX11/04. Script/BulletScript.cpp
+++ DirectX11/04. Script/BulletScript.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "BulletScript.h"
 
+#include "EventQueueScript.h"
+
 CBulletScript::CBulletScript()
 	: CScript((UINT)SCRIPT_TYPE::BULLETSCRIPT)
 	, m_fSpeed(200.f)
@@ -8,6 +10,8 @@ CBulletScript::CBulletScript()
 	, distance(Vec3(0.f, 0.f, 0.f))
 	, isDestroy(false)
 	, isEffectStart(false)
+	, delayTime(0.f)
+	, nowDelayTime(0.f)
 {
 }
 
@@ -21,58 +25,90 @@ void CBulletScript::start()
 
 void CBulletScript::update()
 {
-	Vec3 position = Transform()->GetLocalPos();
-	direction = XMVector2Normalize(direction);
-
-	position.x += direction.x * m_fSpeed * DT;
-	position.y += direction.y * m_fSpeed * DT;
-
-	Transform()->SetLocalPos(position);
-
-	distance.x += direction.x * m_fSpeed * DT;
-	distance.y += direction.y * m_fSpeed * DT;
-
-	if (isEffectStart == false)
+	nowDelayTime += DT;
+	if (nowDelayTime > delayTime)
 	{
-		if (Object()->Animator2D()->GetCurAnim()->IsPlay() == false)
-		{
-			Object()->Animator2D()->GetCurAnim()->Play();
-		}
+		Vec3 position = Transform()->GetLocalPos();
+		direction = XMVector2Normalize(direction);
 
-		Vec3 nowDistance = XMVector2Length(distance);
-		if (nowDistance.x > MAX_DISTANCE)
+		position.x += direction.x * m_fSpeed * DT;
+		position.y += direction.y * m_fSpeed * DT;
+
+		Transform()->SetLocalPos(position);
+
+		distance.x += direction.x * m_fSpeed * DT;
+		distance.y += direction.y * m_fSpeed * DT;
+
+		if (isEffectStart == false)
 		{
-			isEffectStart = true;
-			effectAnimation = Object()->Animator2D()->FindAnim(L"Effect");
-			if (effectAnimation != nullptr)
+			if (Object()->Animator2D()->GetCurAnim()->IsPlay() == false)
 			{
-				Object()->Animator2D()->SetCurAnim(effectAnimation);
 				Object()->Animator2D()->GetCurAnim()->Play();
 			}
+
+			Vec3 nowDistance = XMVector2Length(distance);
+			if (nowDistance.x > MAX_DISTANCE)
+			{
+				isEffectStart = true;
+				effectAnimation = Object()->Animator2D()->FindAnim(L"Effect");
+				if (effectAnimation != nullptr)
+				{
+					Object()->Animator2D()->SetCurAnim(effectAnimation);
+					Object()->Animator2D()->GetCurAnim()->Play();
+				}
+			}
 		}
-	}
-	else
-	{
-		if (effectAnimation != nullptr)
+		else
 		{
-			if (Object()->Animator2D()->GetCurAnim()->IsFinish() == true)
+			if (effectAnimation != nullptr)
+			{
+				if (Object()->Animator2D()->GetCurAnim()->IsFinish() == true)
+				{
+					if (isDestroy == false)
+					{
+						DeleteObject(Object());
+						isDestroy = true;
+						nowDelayTime = 0.f;
+					}
+
+				}
+			}
+			else
 			{
 				if (isDestroy == false)
 				{
 					DeleteObject(Object());
 					isDestroy = true;
+					nowDelayTime = 0.f;
 				}
-
 			}
 		}
-		else
+
+	}
+	else
+	{
+		if (Object()->Animator2D()->GetCurAnim()->IsPlay() == false)
 		{
-			if (isDestroy == false)
-			{
-				DeleteObject(Object());
-				isDestroy = true;
-			}
+			Object()->Animator2D()->GetCurAnim()->Play();
 		}
 	}
+}
 
+void CBulletScript::OnCollisionEnter(CCollider2D * _pOther)
+{
+	if (_pOther->Object()->GetName().compare(L"Player") == 0)
+	{
+		isEffectStart = true;
+		effectAnimation = Object()->Animator2D()->FindAnim(L"Effect");
+		if (effectAnimation != nullptr)
+		{
+			Object()->Animator2D()->SetCurAnim(effectAnimation);
+			Object()->Animator2D()->GetCurAnim()->Play();
+		}
+
+		EVENT_PACKET packet;
+		packet.eventType = EVENTQUEUE_TYPE::HITED;
+		packet.eventSendTargetID = _pOther->Object()->GetID();
+		CEventQueueScript::GetInstance()->EnqueueEvent(packet);
+	}
 }
