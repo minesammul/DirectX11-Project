409623806941e06289f37ead3da72e126ba6045b
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2019-11-14 23:05:29 +0900

    기타 리소스.

--- DirectX11/Project/Include/Engine/Light3D.h
+++ DirectX11/Project/Include/Engine/Light3D.h
@@ -15,7 +15,9 @@ public:
 public:
 	void SetLightType(LIGHT_TYPE _eType);
 	void SetLightDir(const Vec3& _vLightDir) { m_tInfo.vDir = _vLightDir; m_tInfo.vDir.Normalize(); }
-	void SetLightColor(const Vec3& _vLightColor) {m_tInfo.vColor = _vLightColor;}
+	void SetLightDiffuse(const Vec3& _vLightColor) { m_tInfo.vDiff = _vLightColor; }
+	void SetLightSpecular(const Vec3& _vLightSpec) { m_tInfo.vSpec = _vLightSpec; }
+	void SetLightAmbient(const Vec3& _vLightAmb) { m_tInfo.vAmb = _vLightAmb; }
 	void SetLightRange(float _fRange) {m_tInfo.fRange = _fRange;}
 	void SetLightAngle(float _fAngle) { m_tInfo.fAngle = _fAngle; }
 
