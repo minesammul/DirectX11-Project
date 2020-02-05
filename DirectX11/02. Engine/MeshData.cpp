4b47dbf1021c286e78c2cbf52ba680c0d859a1af
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2020-02-05 13:17:47 +0900

    캐릭터와 같은 리소스는 자식 오브젝트에서 충돌체를 지정한다.

--- DirectX11/02. Engine/MeshData.cpp
+++ DirectX11/02. Engine/MeshData.cpp
@@ -204,7 +204,6 @@ CGameObject * CMeshData::Instantiate()
 
 	CGameObject* pRootObj = new CGameObject;
 	pRootObj->AddComponent(new CTransform);
-	//pRootObj->AddComponent(new CMeshRender);
 
 	int meshCount = m_vecMesh.size();
 
