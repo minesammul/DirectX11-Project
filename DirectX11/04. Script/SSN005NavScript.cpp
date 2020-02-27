d8db451358d7470a1d3271196688d1f89f5d2ec5
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2020-02-27 15:18:56 +0900

    Nav Mesh의 충돌이후의 기능.

--- DirectX11/04. Script/SSN005NavScript.cpp
+++ DirectX11/04. Script/SSN005NavScript.cpp
@@ -41,16 +41,77 @@ void CSSN005NavScript::update()
 	isNavCollision = false;
 }
 
+void CSSN005NavScript::OnCollisionEnter(CCollider3D * _pOther)
+{
+	//중력해제
+
+}
+
 void CSSN005NavScript::OnCollision(CCollider3D * _pOther)
 {
+	//충돌지점 이후 위치 정하기
 	beforePosition = Object()->GetParent()->Transform()->GetLocalPos();
 
 	UINT objectID = _pOther->Object()->GetID();
 	navObjectCollisionCheck[objectID] = true;
+
+	{
+		//월드에 존재하는 삼각형 충돌체로부터 평면의 방정식을 만든다.
+		VTX triangleMesh[3] = {};
+		triangleMesh[0].vPos = Vec3(-0.5f, 0.5f, 0.f);
+		triangleMesh[0].vColor = Vec4(1.f, 0.f, 0.f, 1.f);
+		triangleMesh[0].vUV = Vec2(0.f, 0.f);
+
+		triangleMesh[1].vPos = Vec3(0.5f, 0.5f, 0.f);
+		triangleMesh[1].vColor = Vec4(0.f, 1.f, 0.f, 1.f);
+		triangleMesh[1].vUV = Vec2(1.f, 0.f);
+
+		triangleMesh[2].vPos = Vec3(0.5f, -0.5f, 0.f);
+		triangleMesh[2].vColor = Vec4(0.5f, 0.5f, 0.f, 1.f);
+		triangleMesh[2].vUV = Vec2(1.f, 1.f);
+
+		Matrix matTriangleWorld = _pOther->GetWorldMat();
+
+		Vec3 TriangleWorldPos[3] = {};
+
+		for (UINT i = 0; i < 3; ++i)
+		{
+			TriangleWorldPos[i] = XMVector3TransformCoord(triangleMesh[i].vPos, matTriangleWorld);
+		}
+
+		Vec4 triangleEquation = DirectX::XMPlaneFromPoints(TriangleWorldPos[0], TriangleWorldPos[1], TriangleWorldPos[2]);
+		
+		Vec3 triangle0to1VectorIsV1 = TriangleWorldPos[1] - TriangleWorldPos[0];
+		Vec3 triangle1to2VectorIsV2 = TriangleWorldPos[2] - TriangleWorldPos[1];
+		Vec3 triangle2to0VectorIsV3 = TriangleWorldPos[0] - TriangleWorldPos[2];
+
+		Vec3 triangleNormalVector = XMVector3Cross(triangle0to1VectorIsV1, -triangle2to0VectorIsV3);
+		triangleNormalVector.Normalize();
+
+		Vec3 navCollisionWorldPosition = Object()->Transform()->GetWorldPos();
+
+		float wantDistance = Object()->Transform()->GetLocalScale().x - 20.f;
+
+		float changeYPosition = (wantDistance - triangleEquation.x*navCollisionWorldPosition.x - triangleEquation.z*navCollisionWorldPosition.z - triangleEquation.w) / triangleEquation.y;
+		
+		Vec3 characterPosition = Object()->GetParent()->Transform()->GetLocalPos();
+		characterPosition.y = changeYPosition;
+		Object()->GetParent()->Transform()->SetLocalPos(characterPosition);
+	}
+
+	//{
+	//	Vec3 colliderPosition = _pOther->Object()->Transform()->GetLocalPos();
+	//	Vec3 playerPosition = Object()->GetParent()->Transform()->GetLocalPos();
+	//	playerPosition.y = colliderPosition.y;
+	//	Object()->GetParent()->Transform()->SetLocalPos(playerPosition);
+	//}
+
 }
 
 void CSSN005NavScript::OnCollisionExit(CCollider3D * _pOther)
 {
+	//중력적용
+
 	UINT objectID = _pOther->Object()->GetID();
 	navObjectCollisionCheck[objectID] = false;
 }
