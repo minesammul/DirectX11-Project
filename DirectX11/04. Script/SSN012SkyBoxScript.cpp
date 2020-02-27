575b6fa9e91e635583539458676a59e1db552dc6
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2020-02-27 15:14:46 +0900

    SkyBox에 전달되어야 하는 값을 전달하기 위한 스크립트

---
+++ DirectX11/04. Script/SSN012SkyBoxScript.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "SSN012SkyBoxScript.h"
+
+
+CSSN012SkyBoxScript::CSSN012SkyBoxScript() : 
+	CScript((UINT)SCRIPT_TYPE::SSN012SKYBOXSCRIPT)
+{
+}
+
+
+CSSN012SkyBoxScript::~CSSN012SkyBoxScript()
+{
+}
+
+void CSSN012SkyBoxScript::update()
+{
+	CResPtr<CTexture> pSkyBoxTex = CResMgr::GetInst()->FindRes<CTexture>(L"Texture\\Skybox\\Sky01.png");
+	Object()->MeshRender()->GetSharedMaterial()->SetData(SHADER_PARAM::TEX_0, &pSkyBoxTex);
+}
---
+++ DirectX11/Project/Include/Script/SSN012SkyBoxScript.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Script.h>
+
+class CSSN012SkyBoxScript : public CScript
+{
+public:
+	CSSN012SkyBoxScript();
+	~CSSN012SkyBoxScript();
+
+public:
+	virtual void update();
+};
+
