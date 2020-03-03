3193bca2f72969456c99731242a0112167d82288
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2020-03-03 18:28:29 +0900

    쓸데없는 주석 제거 리팩토링.

--- DirectX11/04. Script/SSN009HitBoxScript.h
+++ DirectX11/04. Script/SSN009HitBoxScript.h
@@ -3,18 +3,11 @@
 
 class CSSN009HitBoxScript : public CScript
 {
-private:
-	//bool isCollision;
-
-
 public:
 	CSSN009HitBoxScript();
 	~CSSN009HitBoxScript();
 
 public:
 	virtual void OnCollision(CCollider3D* _pOther);
-
-	//bool GetCollision() { return isCollision; }
-	//void SetCollision(bool collision) { isCollision = collision; }
 };
 
