a6fa0907e13ea951e95fdea200f476617c50a83d
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2020-03-09 23:24:14 +0900

    외부, 내부로 이동할 때 광량조절 효과 추가.

--- DirectX11/04. Script/SSN014DirectionLightScript.cpp
+++ DirectX11/04. Script/SSN014DirectionLightScript.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "SSN014DirectionLightScript.h"
 
+#include <Light3D.h>
 #include "FunctionMgr.h"
 
 CSSN014DirectionLightScript::CSSN014DirectionLightScript():
@@ -13,13 +14,62 @@ CSSN014DirectionLightScript::~CSSN014DirectionLightScript()
 {
 }
 
-void CSSN014DirectionLightScript::start()
+void CSSN014DirectionLightScript::DownLightVoluem()
 {
-	mPlayer = CFunctionMgr::GetInst()->FindObject(L"Player");
-	mPlayerBeforePosition = mPlayer->Transform()->GetLocalPos();
+	if (mIsLightVoluemDown == true)
+	{
+		float voluem = CTimeMgr::GetInst()->GetDeltaTime()*mChangeSpeed;
+		Vec3 lightVolume = Vec3(voluem, voluem, voluem);
+
+		Vec3 ambientVoluem = Vec3(	Object()->Light3D()->GetLight3DInfo().vAmb.x,
+									Object()->Light3D()->GetLight3DInfo().vAmb.y,
+									Object()->Light3D()->GetLight3DInfo().vAmb.z);
+
+		Vec3 diffuseVoluem = Vec3(	Object()->Light3D()->GetLight3DInfo().vDiff.x,
+									Object()->Light3D()->GetLight3DInfo().vDiff.y,
+									Object()->Light3D()->GetLight3DInfo().vDiff.z);
+
+		Vec3 specularVoluem = Vec3(	Object()->Light3D()->GetLight3DInfo().vSpec.x,
+									Object()->Light3D()->GetLight3DInfo().vSpec.y,
+									Object()->Light3D()->GetLight3DInfo().vSpec.z);
+
+		diffuseVoluem += lightVolume;
+		ambientVoluem += lightVolume;
+		specularVoluem += lightVolume;
+
+		bool isAmbientDone = false;
+		if (ambientVoluem.x >= mGoalAmbient.x)
+		{
+			ambientVoluem = mGoalAmbient;
+			isAmbientDone = true;
+		}
+
+		bool isDiffuseDone = false;
+		if (diffuseVoluem.x >= mGoalDiffsue.x)
+		{
+			diffuseVoluem = mGoalDiffsue;
+			isDiffuseDone = true;
+		}
+
+		bool isSpecularDone = false;
+		if (specularVoluem.x >= mGoalSpecular.x)
+		{
+			diffuseVoluem = mGoalSpecular;
+			isSpecularDone = true;
+		}
+
+		Object()->Light3D()->SetLightAmbient(ambientVoluem);
+		Object()->Light3D()->SetLightDiffuse(diffuseVoluem);
+		Object()->Light3D()->SetLightSpecular(specularVoluem);
+
+		if (isDiffuseDone == true && isAmbientDone == true && isSpecularDone == true)
+		{
+			mIsLightVoluemDown = false;
+		}
+	}
 }
 
-void CSSN014DirectionLightScript::update()
+void CSSN014DirectionLightScript::UpdatePosition()
 {
 	Vec3 playerNowPosition = mPlayer->Transform()->GetLocalPos();
 	if (fabsf(playerNowPosition.x - mPlayerBeforePosition.x) > 1.f)
@@ -41,4 +91,30 @@ void CSSN014DirectionLightScript::update()
 
 		mPlayerBeforePosition.z = playerNowPosition.z;
 	}
+}
+
+void CSSN014DirectionLightScript::start()
+{
+	mPlayer = CFunctionMgr::GetInst()->FindObject(L"Player");
+	mPlayerBeforePosition = mPlayer->Transform()->GetLocalPos();
+}
+
+void CSSN014DirectionLightScript::update()
+{
+	UpdatePosition();
+	DownLightVoluem();
+}
+
+void CSSN014DirectionLightScript::StartLightVoluemDown(Vec3 startAmbient, Vec3 startDiffuse, Vec3 startSpecular, Vec3 goalAmbient, Vec3 goalDiffuse, Vec3 goalSpecular, float changeSpeed)
+{
+	mIsLightVoluemDown = true;
+
+	Object()->Light3D()->SetLightAmbient(startAmbient);
+	Object()->Light3D()->SetLightDiffuse(startDiffuse);
+	Object()->Light3D()->SetLightSpecular(startDiffuse);
+
+	mGoalAmbient = goalAmbient;
+	mGoalDiffsue = goalDiffuse;
+	mGoalSpecular = goalSpecular;
+	mChangeSpeed = changeSpeed;
 }
--- DirectX11/04. Script/SSN014DirectionLightScript.h
+++ DirectX11/04. Script/SSN014DirectionLightScript.h
@@ -7,12 +7,25 @@ private:
 	Vec3 mPlayerBeforePosition;
 	CGameObject* mPlayer;
 
+	bool mIsLightVoluemDown;
+
+	Vec3 mGoalAmbient;
+	Vec3 mGoalDiffsue;
+	Vec3 mGoalSpecular;
+	float mChangeSpeed;
+
 public:
 	CSSN014DirectionLightScript();
 	~CSSN014DirectionLightScript();
 
+private:
+	void DownLightVoluem();
+	void UpdatePosition();
+
 public:
 	virtual void start();
 	virtual void update();
+
+	void StartLightVoluemDown(Vec3 startAmbient, Vec3 startDiffuse, Vec3 startSpecular, Vec3 goalAmbient, Vec3 goalDiffuse, Vec3 goalSpecular, float changeSpeed);
 };
 
