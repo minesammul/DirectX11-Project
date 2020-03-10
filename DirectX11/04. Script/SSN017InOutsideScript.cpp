8046287dee80117f11589bbf5a90e4ecb67749a0
DESKTOP-ILHMG2U\user <minesammul@naver.com>
2020-03-10 16:57:10 +0900

    외부, 내부 명암조절 기능 추가.

---
+++ DirectX11/04. Script/SSN017InOutsideScript.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "SSN017InOutsideScript.h"
+
+
+CSSN017InOutsideScript::CSSN017InOutsideScript():
+	CScript((UINT)SCRIPT_TYPE::SSN017INOUTSIDESCRIPT)
+{
+}
+
+
+CSSN017InOutsideScript::~CSSN017InOutsideScript()
+{
+}
+
+void CSSN017InOutsideScript::start()
+{
+	if (Object()->GetName().find(L"ToInside") != wstring::npos)
+	{
+		Object()->MeshRender()->SetMaterial(CResMgr::GetInst()->FindRes<CMaterial>(L"UIMtrl"));
+
+		float alpha = 0.7f;
+		CResPtr<CTexture> texture = CResMgr::GetInst()->FindRes<CTexture>(L"Texture\\UI\\Inside.png");
+		Object()->MeshRender()->GetCloneMaterial()->SetData(SHADER_PARAM::FLOAT_0, &alpha);
+		Object()->MeshRender()->GetCloneMaterial()->SetData(SHADER_PARAM::TEX_0, &texture);
+	}
+}
---
+++ DirectX11/04. Script/SSN017InOutsideScript.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Script.h>
+
+class CSSN017InOutsideScript : public CScript
+{
+public:
+	CSSN017InOutsideScript();
+	~CSSN017InOutsideScript();
+
+public:
+	virtual void start();
+};
+
